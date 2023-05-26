1b13953f72af226490ead501b627005fb600772d
Cinzia Pulvirenti <cinzia2092@hotmail.it>
2023-05-26 15:24:42 +0200

    fix file storage

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -5,6 +5,14 @@
 
     <form action="{{route('products.store')}}" method="POST" enctype="multipart/form-data">
         @csrf
+
+        <div class="mb-3">
+            <label for="thumb" class="form-label">Immagine prodotto</label>
+            <input type="file" class="form-control @error('thumb') is-invalid @enderror" value="{{old('thumb')}}"  id="thumb" name="thumb">
+                @error('thumb')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+        </div>
         
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">Nome piatto</label>
@@ -24,14 +32,6 @@
             <label for="exampleFormControlInput1" class="form-label">Prezzo</label>
             <input type="text" class="form-control @error('price') is-invalid @enderror" value="{{old('price')}}" id="exampleFormControlInput1" name="price">
                 @error('price')
-                <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
-        </div>
-
-        <div class="mb-3">
-            <label for="thumb" class="form-label">Immagine prodotto</label>
-            <input type="file" class="form-control @error('thumb') is-invalid @enderror" value="{{old('thumb')}}"  id="thumb" name="thumb">
-                @error('thumb')
                 <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
         </div>
