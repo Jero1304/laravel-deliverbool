03dfd053c0e8e16c72e58e3cff9f195b8b23b207
marypaduano <maria.paduano@gmail.com>
2023-05-30 16:30:52 +0200

    add validate edit product

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -9,21 +9,21 @@
         
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">Nome piatto</label>
-            <input type="text" class="form-control @error('name') is-invalid @enderror" value="{{old('name', $product['name'])}}"  id="exampleFormControlInput1" name="name">
+            <input type="text" pattern="[A-Za-z ]+" title="Inserisci un nome valido (solo lettere)" class="form-control @error('name') is-invalid @enderror" value="{{old('name', $product['name'])}}"  id="exampleFormControlInput1" name="name" required>
                 @error('name')
                 <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
         </div>
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">Ingredienti</label>
-            <input type="text" class="form-control @error('ingredient') is-invalid @enderror" value="{{old('ingredient', $product['ingredient'])}}"  id="exampleFormControlInput1" name="ingredient">
+            <input type="text" pattern="[A-Za-z ]+" title="Inserisci testo valido (solo lettere separate da spazio)" class="form-control @error('ingredient') is-invalid @enderror" value="{{old('ingredient', $product['ingredient'])}}"  id="exampleFormControlInput1" name="ingredient" required>
                 @error('ingredient')
                 <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
         </div>
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">Prezzo</label>
-            <input type="text" class="form-control @error('price') is-invalid @enderror" value="{{old('price', $product['price'])}}" id="exampleFormControlInput1" name="price">
+            <input type="text" pattern="^\d+(\.\d{1,2})?" title="Inserisci un prezzo valido (es. 10.99)" required class="form-control @error('price') is-invalid @enderror" value="{{old('price', $product['price'])}}" id="exampleFormControlInput1" name="price">
                 @error('price')
                 <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
@@ -31,18 +31,19 @@
 
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">Immagine</label>
-            <input type="text" class="form-control @error('thumb') is-invalid @enderror" value="{{old('thumb', $product['thumb'])}}" id="exampleFormControlInput1" name="thumb">
+            <input type="text" accept="image/*" required class="form-control @error('thumb') is-invalid @enderror" value="{{old('thumb', $product['thumb'])}}" id="exampleFormControlInput1" name="thumb">
                 @error('thumb')
                 <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
         </div>
 
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Visibile</label>
-            <input type="text" class="form-control @error('visible') is-invalid @enderror" value="{{old('visible', $product['visible'])}}" id="exampleFormControlInput1" name="visible">
-                @error('visible')
-                <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
+        <div class="form-check form-switch">
+            <input class="form-check-input @error('visible') is-invalid @enderror" value="{{old('visible')}}" id="exampleFormControlInput1" name="visible" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked required title="seleziona il campo">
+            <label class="form-check-label mb-5" for="flexSwitchCheckChecked">Visibile</label>
+            @error('visible')
+            <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+            {{-- aggiungere validazione del check --}}
         </div>
 
     <button type="submit" class="btn btn-primary">Crea</button>
