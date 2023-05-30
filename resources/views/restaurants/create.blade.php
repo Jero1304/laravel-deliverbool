826c8ca42b6dd651edb10f5fae7493bec829c6a2
marypaduano <maria.paduano@gmail.com>
2023-05-30 16:35:17 +0200

    add validate create restaurant

--- resources/views/restaurants/create.blade.php
+++ resources/views/restaurants/create.blade.php
@@ -8,28 +8,28 @@
         
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">Nome ristorante</label>
-            <input type="text" class="form-control @error('restaurant_name') is-invalid @enderror" value="{{old('restaurant_name')}}"  id="exampleFormControlInput1" name="restaurant_name">
+            <input type="text" pattern="[A-Za-z ]+" title="Inserisci un nome valido (solo lettere)" required class="form-control @error('restaurant_name') is-invalid @enderror" value="{{old('restaurant_name')}}"  id="exampleFormControlInput1" name="restaurant_name">
                 @error('restaurant_name')
                 <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
         </div>
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">Indirizzo</label>
-            <input type="text" class="form-control @error('address') is-invalid @enderror" value="{{old('address')}}"  id="exampleFormControlInput1" name="address">
+            <input type="text" pattern="[A-Za-z ]+" title="Inserisci un indirizzo" class="form-control @error('address') is-invalid @enderror" value="{{old('address')}}"  id="exampleFormControlInput1" name="address" required>
                 @error('address')
                 <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
         </div>
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">Partita IVA</label>
-            <input type="text" class="form-control @error('vat') is-invalid @enderror" value="{{old('vat')}}" id="exampleFormControlInput1" name="vat">
+            <input type="text" pattern="[0-9]{11}" title="Inserisci una partita IVA valida (11 numeri)" required class="form-control @error('vat') is-invalid @enderror" value="{{old('vat')}}" id="exampleFormControlInput1" name="vat">
                 @error('vat')
                 <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
         </div>
     @foreach($types as $key => $type)
         <div class="form-check">
-            <input name="types[]" @checked( in_array($type->id, old('types',[]) ) ) class="form-check-input" type="checkbox" value="{{ $type->id }}" id="flexCheckDefault">
+            <input name="types[]" @checked( in_array($type->id, old('types',[]) ) ) class="form-check-input" type="checkbox" value="{{ $type->id }}" id="flexCheckDefault" required>
             <label class="form-check-label" for="flexCheckDefault">
             {{ $type->name }}
             </label>
